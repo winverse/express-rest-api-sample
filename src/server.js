068030866d4b270145d5fc8f2233b8aa17f4e527
sangHyun <public.winverse@gmail.com>
2021-03-06 18:21:43 +0900

    Add Dotenv

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ require('./env');
 const app = require('app');
 
 // 비구조화 할당 금지 cosnt { PORT } = process.env; (X)
-const PROT = process.env.PORT;
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  throw new Error('MISSING_EVVAR');
+}
 
 // 서버 동작
-const server = app.listen(PROT, () => {
-  console.log(`Server is running, port number is ${PROT} `);
+const server = app.listen(PORT, () => {
+  console.log(`Server is running, port number is ${PORT} `);
 });
 
 module.exports = server;
